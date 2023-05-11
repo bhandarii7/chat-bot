babf1e7a8591affa12f1448a13a16f92c4928928
Yash Bhandari <yashbhandari5018@gmail.com>
2023-05-11 12:41:54 +0530

    chat-bot

---
+++ src/components/Chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Chat() {
+  return (
+    <>
+<df-messenger
+  intent="WELCOME"
+  chat-title="fdcc"
+  agent-id="cbed9cf5-c644-4e06-a264-10fa884f63b0"
+  language-code="en"
+></df-messenger>
+    </>
+  )
+}
